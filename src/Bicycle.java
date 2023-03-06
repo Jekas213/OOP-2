e2a71bba136d153465d97590053e7c10a7549dad
Dmitriy <jekas213@gmail.com>
2023-03-06 22:31:00 +0300

    на проверку

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,9 +1,17 @@
-public class Bicycle {
+public class Bicycle extends Machine implements ServiceStation {
 
-    public String modelName;
-    public int wheelsCount;
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
 
+    @Override
     public void updateTyre() {
         System.out.println("Меняем покрышку");
+    }
+
+    @Override
+    public void serviceStation() {
+        System.out.println("Обслуживаем " + getModelName());
+        updateTyre();
     }
 }
--- src/Car.java
+++ src/Car.java
@@ -1,13 +1,22 @@
-public class Car {
+public class Car extends Machine implements ServiceStation {
 
-    public String modelName;
-    public int wheelsCount;
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
 
+    @Override
     public void updateTyre() {
         System.out.println("Меняем покрышку");
     }
 
     public void checkEngine() {
         System.out.println("Проверяем двигатель");
+    }
+
+    @Override
+    public void serviceStation() {
+        System.out.println("Обслуживаем " + getModelName());
+        updateTyre();
+        checkEngine();
     }
 }
